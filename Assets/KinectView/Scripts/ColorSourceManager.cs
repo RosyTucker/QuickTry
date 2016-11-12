d07e48260bfa16e0d3f1a41e03d9d0d87354fa89
Your Name <rosy533@gmail.com>
2016-11-12 10:43:52 +0000

    Initial commit

---
+++ Assets/KinectView/Scripts/ColorSourceManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class ColorSourceManager : MonoBehaviour 
+{
+    public int ColorWidth { get; private set; }
+    public int ColorHeight { get; private set; }
+
+    private KinectSensor _sensor;
+    private ColorFrameReader _reader;
+    private Texture2D _texture;
+    private byte[] _data;
+    
+    public Texture2D GetColorTexture()
+    {
+        return _texture;
+    }
+    
+    void Start()
+    {
+        _sensor = KinectSensor.GetDefault();
+
+        if (_sensor == null) return;
+
+        _reader = _sensor.ColorFrameSource.OpenReader();
+            
+        var frameDesc = _sensor.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Rgba);
+        ColorWidth = frameDesc.Width;
+        ColorHeight = frameDesc.Height;
+            
+        _texture = new Texture2D(frameDesc.Width, frameDesc.Height, TextureFormat.RGBA32, false);
+        _data = new byte[frameDesc.BytesPerPixel * frameDesc.LengthInPixels];
+            
+        if (!_sensor.IsOpen)
+        {
+            _sensor.Open();
+        }
+    }
+    
+    void Update ()
+    {
+        if (_reader == null) return;
+        using (var frame = _reader.AcquireLatestFrame())
+        {
+            if (frame == null) return;
+            frame.CopyConvertedFrameDataToArray(_data, ColorImageFormat.Rgba);
+            _texture.LoadRawTextureData(_data);
+            _texture.Apply();
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (_reader != null) _reader.Dispose();
+
+        if (_sensor != null && _sensor.IsOpen) _sensor.Close();
+            
+        _sensor = null;
+        _reader = null;
+    }
+}
---
+++ Assets/KinectView/Scripts/ColorSourceView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class ColorSourceView : MonoBehaviour
+{
+    public GameObject ColorSourceManager;
+    private ColorSourceManager _colorManager;
+    
+    void Start ()
+    {
+        gameObject.GetComponent<Renderer>().material.SetTextureScale("_MainTex", new Vector2(-1, 1));
+    }
+    
+    void Update()
+    {
+        if (ColorSourceManager == null) return;
+        
+        _colorManager = ColorSourceManager.GetComponent<ColorSourceManager>();
+
+        if (_colorManager == null) return;
+        
+        gameObject.GetComponent<Renderer>().material.mainTexture = _colorManager.GetColorTexture();
+    }
+}
