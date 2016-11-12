f5a5a49e7ef80b0fca97c82db9946816b3a50032
Your Name <rosy533@gmail.com>
2016-11-12 18:15:00 +0000

    Fix Infinity exceptions

--- Assets/KinectView/Scripts/BodySourceView.cs
+++ Assets/KinectView/Scripts/BodySourceView.cs
@@ -1,119 +1,116 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections.Generic;
 using Assets.KinectView.Scripts;
 using Assets.KinectView.Util;
 using Kinect = Windows.Kinect;
 
-public class BodySourceView : MonoBehaviour 
+public class BodySourceView : MonoBehaviour
 {
     public Material BoneMaterial;
     public GameObject BodySourceManager;
-    
+
     private Dictionary<ulong, GameObject> _Bodies = new Dictionary<ulong, GameObject>();
     private BodySourceManager _BodyManager;
-    
-    private Dictionary<Kinect.JointType, Kinect.JointType> _BoneMap = new Dictionary<Kinect.JointType, Kinect.JointType>()
-    {
-        { Kinect.JointType.FootLeft, Kinect.JointType.AnkleLeft },
-        { Kinect.JointType.AnkleLeft, Kinect.JointType.KneeLeft },
-        { Kinect.JointType.KneeLeft, Kinect.JointType.HipLeft },
-        { Kinect.JointType.HipLeft, Kinect.JointType.SpineBase },
-        
-        { Kinect.JointType.FootRight, Kinect.JointType.AnkleRight },
-        { Kinect.JointType.AnkleRight, Kinect.JointType.KneeRight },
-        { Kinect.JointType.KneeRight, Kinect.JointType.HipRight },
-        { Kinect.JointType.HipRight, Kinect.JointType.SpineBase },
-        
-        { Kinect.JointType.HandTipLeft, Kinect.JointType.HandLeft },
-        { Kinect.JointType.ThumbLeft, Kinect.JointType.HandLeft },
-        { Kinect.JointType.HandLeft, Kinect.JointType.WristLeft },
-        { Kinect.JointType.WristLeft, Kinect.JointType.ElbowLeft },
-        { Kinect.JointType.ElbowLeft, Kinect.JointType.ShoulderLeft },
-        { Kinect.JointType.ShoulderLeft, Kinect.JointType.SpineShoulder },
-        
-        { Kinect.JointType.HandTipRight, Kinect.JointType.HandRight },
-        { Kinect.JointType.ThumbRight, Kinect.JointType.HandRight },
-        { Kinect.JointType.HandRight, Kinect.JointType.WristRight },
-        { Kinect.JointType.WristRight, Kinect.JointType.ElbowRight },
-        { Kinect.JointType.ElbowRight, Kinect.JointType.ShoulderRight },
-        { Kinect.JointType.ShoulderRight, Kinect.JointType.SpineShoulder },
-        
-        { Kinect.JointType.SpineBase, Kinect.JointType.SpineMid },
-        { Kinect.JointType.SpineMid, Kinect.JointType.SpineShoulder },
-        { Kinect.JointType.SpineShoulder, Kinect.JointType.Neck },
-        { Kinect.JointType.Neck, Kinect.JointType.Head },
-    };
-    
-    void Update () 
+
+    private Dictionary<Kinect.JointType, Kinect.JointType> _BoneMap = new Dictionary<Kinect.JointType, Kinect.JointType>
+        ()
+        {
+            {Kinect.JointType.FootLeft, Kinect.JointType.AnkleLeft},
+            {Kinect.JointType.AnkleLeft, Kinect.JointType.KneeLeft},
+            {Kinect.JointType.KneeLeft, Kinect.JointType.HipLeft},
+            {Kinect.JointType.HipLeft, Kinect.JointType.SpineBase},
+            {Kinect.JointType.FootRight, Kinect.JointType.AnkleRight},
+            {Kinect.JointType.AnkleRight, Kinect.JointType.KneeRight},
+            {Kinect.JointType.KneeRight, Kinect.JointType.HipRight},
+            {Kinect.JointType.HipRight, Kinect.JointType.SpineBase},
+            {Kinect.JointType.HandTipLeft, Kinect.JointType.HandLeft},
+            {Kinect.JointType.ThumbLeft, Kinect.JointType.HandLeft},
+            {Kinect.JointType.HandLeft, Kinect.JointType.WristLeft},
+            {Kinect.JointType.WristLeft, Kinect.JointType.ElbowLeft},
+            {Kinect.JointType.ElbowLeft, Kinect.JointType.ShoulderLeft},
+            {Kinect.JointType.ShoulderLeft, Kinect.JointType.SpineShoulder},
+            {Kinect.JointType.HandTipRight, Kinect.JointType.HandRight},
+            {Kinect.JointType.ThumbRight, Kinect.JointType.HandRight},
+            {Kinect.JointType.HandRight, Kinect.JointType.WristRight},
+            {Kinect.JointType.WristRight, Kinect.JointType.ElbowRight},
+            {Kinect.JointType.ElbowRight, Kinect.JointType.ShoulderRight},
+            {Kinect.JointType.ShoulderRight, Kinect.JointType.SpineShoulder},
+            {Kinect.JointType.SpineBase, Kinect.JointType.SpineMid},
+            {Kinect.JointType.SpineMid, Kinect.JointType.SpineShoulder},
+            {Kinect.JointType.SpineShoulder, Kinect.JointType.Neck},
+            {Kinect.JointType.Neck, Kinect.JointType.Head},
+        };
+
+    void Update()
     {
         if (BodySourceManager == null)
         {
             return;
         }
-        
+
         _BodyManager = BodySourceManager.GetComponent<BodySourceManager>();
         if (_BodyManager == null)
         {
             return;
         }
-        
+
         var bodyData = _BodyManager.BodyViewModels;
         if (bodyData == null)
         {
             return;
         }
-        
+
         var trackedIds = new List<ulong>();
-        foreach(var body in bodyData)
+        foreach (var body in bodyData)
         {
             if (body == null)
             {
                 continue;
-              }
-                
-            if(body.IsTracked)
+            }
+
+            if (body.IsTracked)
             {
-                trackedIds.Add (body.TrackingId);
+                trackedIds.Add(body.TrackingId);
             }
         }
-        
+
         var knownIds = new List<ulong>(_Bodies.Keys);
-        
+
         // First delete untracked bodies
-        foreach(var trackingId in knownIds)
+        foreach (var trackingId in knownIds)
         {
-            if(!trackedIds.Contains(trackingId))
+            if (!trackedIds.Contains(trackingId))
             {
                 Destroy(_Bodies[trackingId]);
                 _Bodies.Remove(trackingId);
             }
         }
 
-        foreach(var body in bodyData)
+        foreach (var body in bodyData)
         {
             if (body == null)
             {
                 continue;
             }
-            
-            if(body.IsTracked)
+
+            if (!body.IsTracked) continue;
+
+            if (!_Bodies.ContainsKey(body.TrackingId))
             {
-                if(!_Bodies.ContainsKey(body.TrackingId))
-                {
-                    _Bodies[body.TrackingId] = CreateBodyObject(body.TrackingId);
-                }
-                
-                RefreshBodyObject(body, _Bodies[body.TrackingId]);
+                _Bodies[body.TrackingId] = CreateBodyObject(body.TrackingId);
             }
+
+            RefreshBodyObject(body, _Bodies[body.TrackingId]);
         }
     }
-    
+
     private GameObject CreateBodyObject(ulong id)
     {
         var body = new GameObject("Body:" + id);
         body.transform.parent = gameObject.transform;
 
-        for (Kinect.JointType jt = Kinect.JointType.SpineBase; jt <= Kinect.JointType.ThumbRight; jt++)
+        foreach (Kinect.JointType joint in Enum.GetValues(typeof(Kinect.JointType)))
         {
             var jointObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
@@ -121,30 +118,25 @@ public class BodySourceView : MonoBehaviour
             lr.SetVertexCount(2);
             lr.material = BoneMaterial;
             lr.SetWidth(0.05f, 0.05f);
-            
-            jointObj.name = jt.ToString();
+
+            jointObj.name = joint.ToString();
             jointObj.transform.parent = body.transform;
         }
-        
+
         return body;
     }
-    
+
     private void RefreshBodyObject(BodyViewModel body, GameObject bodyObject)
     {
-        for (var joint = Kinect.JointType.SpineBase; joint <= Kinect.JointType.ThumbRight; joint++)
+        foreach (Kinect.JointType joint in _BoneMap.Keys)
         {
             var sourceJoint = body.Joints[joint];
-            JointViewModel targetJoint = null;
-            
-            if(_BoneMap.ContainsKey(joint))
-            {
-                targetJoint = body.Joints[_BoneMap[joint]];
-            }
-            
+            var targetJoint = body.Joints[_BoneMap[joint]];
+
             var jointObj = bodyObject.transform.FindChild(joint.ToString());
             jointObj.localPosition = GetVector3FromJoint(sourceJoint);
-            
-            if(targetJoint != null)
+
+            if (targetJoint != null)
             {
                 DrawBone(jointObj, sourceJoint, targetJoint);
             }
@@ -168,9 +160,16 @@ public class BodySourceView : MonoBehaviour
     }
 
 
-    private static float MapToUiCoordinates(float numberToMap, float minInput, float maxInput, float minOutput, float maxOutput)
+    private static float MapToUiCoordinates(float numberToMap, float minInput, float maxInput, float minOutput,
+        float maxOutput)
     {
-    return (numberToMap - minInput) * (maxOutput - minOutput) / (maxInput - minInput) + minOutput;
+        var output = (numberToMap - minInput)*(maxOutput - minOutput)/(maxInput - minInput) + minOutput;
+        return LimitInclusive(output, minOutput, maxOutput);
+    }
+
+    public static float LimitInclusive(float value, float min, float max)
+    {
+        return Math.Min(max, Math.Max(value, min));
     }
 
     private static Color GetColorForState(Kinect.TrackingState state)
