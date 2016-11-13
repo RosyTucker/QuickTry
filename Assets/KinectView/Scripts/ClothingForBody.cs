bd780e2d852d8a356279d805f85be4687c4b8295
Your Name <rosy533@gmail.com>
2016-11-13 16:02:37 +0000

    Split out clothingforbody wrapper

---
+++ Assets/KinectView/Scripts/ClothingForBody.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.KinectView.Scripts
+{
+    public class ClothingForBody
+    {
+        public GameObject BodyObject { get; set; }
+        public Dictionary<string, GameObject> Clothes { get; set; }
+        public readonly ulong BodyTrackingId;
+
+        public ClothingForBody(GameObject bodyObject, ulong bodyTrackingId)
+        {
+            BodyObject = bodyObject;
+            Clothes = new Dictionary<string, GameObject>();
+            BodyTrackingId = bodyTrackingId;
+        }
+    }
+}
--- Assets/KinectView/Scripts/ClothingView.cs
+++ Assets/KinectView/Scripts/ClothingView.cs
@@ -6,87 +6,73 @@ using UnityEngine;
 
 namespace Assets.KinectView.Scripts
 {
-    public class ClothingWrapper
-    {
-        public GameObject WrapperObject { get; set; }
-        public Dictionary<string, GameObject> Clothes { get; set; }
-        public readonly ulong TrackingId;
-
-        public ClothingWrapper(GameObject wrapperObject, ulong trackingId)
-        {
-            WrapperObject = wrapperObject;
-            Clothes = new Dictionary<string, GameObject>();
-            TrackingId = trackingId;
-        }
-    }
-
     public class ClothingView : MonoBehaviour
     {
         public GameObject ClothingManager;
         public GameObject BodySourceManager;
 
-        private readonly Dictionary<ulong, ClothingWrapper> _clothing = new Dictionary<ulong, ClothingWrapper>();
+        private readonly Dictionary<ulong, ClothingForBody> _clothingForBodies = new Dictionary<ulong, ClothingForBody>();
 
         void Update()
         {
             var bodyData = GameObjectUtils.GetUpdatedBodyData(BodySourceManager);
-            if (bodyData == null) return;
-
             var assignedClothing = GameObjectUtils.GetAssignedClothing(ClothingManager);
+
+            if (bodyData == null) return;
             var trackedBodyData = bodyData.Where(body => body.IsTracked).ToArray();
 
-            var knownBodyIds = new List<ulong>(_clothing.Keys);
+            var knownBodyIds = new List<ulong>(_clothingForBodies.Keys);
             var freshBodyIds = trackedBodyData.Select(trackedBody => trackedBody.TrackingId).ToList();
             RemoveDeadBodies(knownBodyIds, freshBodyIds);
 
             foreach (var body in trackedBodyData)
             {
-                if (!_clothing.ContainsKey(body.TrackingId))
+                if (!_clothingForBodies.ContainsKey(body.TrackingId))
                 {
-                    _clothing[body.TrackingId] = CreateClothingBody(body);
+                    _clothingForBodies[body.TrackingId] = CreateClothingBody(body);
                 }
-                UpdateClothesForBody(body, _clothing[body.TrackingId], assignedClothing);
+                UpdateClothesForBody(body, _clothingForBodies[body.TrackingId], assignedClothing);
             }
         }
 
-        private ClothingWrapper CreateClothingBody(BodyViewModel body)
+        private ClothingForBody CreateClothingBody(BodyViewModel body)
         {
             var bodyClothingObject = new GameObject("BodyClothing:" + body.TrackingId);
             bodyClothingObject.transform.parent = gameObject.transform;
-            return new ClothingWrapper(bodyClothingObject, body.TrackingId);
+            return new ClothingForBody(bodyClothingObject, body.TrackingId);
         }
 
-        private void UpdateClothesForBody(BodyViewModel body, ClothingWrapper clothingWrapper,
+        private void UpdateClothesForBody(BodyViewModel body, ClothingForBody clothingForBody,
             Dictionary<string, ClothingItem> newClothes)
         {
-            RemoveDeadClothes(new List<string>(newClothes.Keys), clothingWrapper);
+            RemoveDeadClothes(new List<string>(newClothes.Keys), clothingForBody);
             foreach (var clothingItem in newClothes.Values)
             {
-                if (!clothingWrapper.Clothes.ContainsKey(clothingItem.Id))
+                if (!clothingForBody.Clothes.ContainsKey(clothingItem.Id))
                 {
                     var clothingObject = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-                    clothingObject.transform.parent = clothingWrapper.WrapperObject.transform;
+                    clothingObject.transform.parent = clothingForBody.BodyObject.transform;
                     clothingObject.transform.localScale = new Vector3(12, 15, 12);
                     clothingObject.name = clothingItem.Id;
-                    clothingWrapper.Clothes.Add(clothingItem.Id, clothingObject);
+                    clothingForBody.Clothes.Add(clothingItem.Id, clothingObject);
                 }
-                clothingWrapper.Clothes[clothingItem.Id].transform.localPosition =
+                clothingForBody.Clothes[clothingItem.Id].transform.localPosition =
                     transform.LocalPositionFromColorSourcePosition(body.Joints[JointType.SpineBase].Position);
             }
         }
 
-        private void RemoveDeadClothes(List<string> newClothingIds, ClothingWrapper clothingWrapper)
+        private void RemoveDeadClothes(List<string> newClothingIds, ClothingForBody clothingForBody)
         {
-            var existingClothingIds = new List<string>(clothingWrapper.Clothes.Keys);
+            var existingClothingIds = new List<string>(clothingForBody.Clothes.Keys);
             foreach (var knownClothingId in existingClothingIds)
             {
                 if (newClothingIds.Contains(knownClothingId)) continue;
 
-                var objectToRemove = clothingWrapper.Clothes[knownClothingId];
+                var objectToRemove = clothingForBody.Clothes[knownClothingId];
 
                 Destroy(objectToRemove);
 
-                clothingWrapper.Clothes.Remove(knownClothingId);
+                clothingForBody.Clothes.Remove(knownClothingId);
             }
         }
 
@@ -96,15 +82,15 @@ namespace Assets.KinectView.Scripts
             {
                 if (freshBodyIds.Contains(knownBodyId)) continue;
 
-                var wrapper = _clothing[knownBodyId];
+                var wrapper = _clothingForBodies[knownBodyId];
                 foreach (var clothingItem in wrapper.Clothes.Values)
                 {
                     Destroy(clothingItem);
                 }
 
-                Destroy(wrapper.WrapperObject);
+                Destroy(wrapper.BodyObject);
 
-                _clothing.Remove(knownBodyId);
+                _clothingForBodies.Remove(knownBodyId);
             }
         }
     }
